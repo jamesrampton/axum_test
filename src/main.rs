8227675fe778ba7be31cafefbd0389e2db9ac7c8
James Rampton <me@jamesrampton.com>
2023-03-03 12:41:14 +0000

    Try out tracing console

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use std::net::{Ipv4Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let subscriber = tracing_subscriber::fmt()
-        .with_file(true)
-        .with_line_number(true)
-        .with_target(true)
-        .with_thread_names(true)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)?;
+    // let subscriber = tracing_subscriber::fmt()
+    //     .with_file(true)
+    //     .with_line_number(true)
+    //     .with_target(true)
+    //     .with_thread_names(true)
+    //     .finish();
+    // tracing::subscriber::set_global_default(subscriber)?;
+    console_subscriber::init();
 
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 7878));
     axum::Server::bind(&addr)
