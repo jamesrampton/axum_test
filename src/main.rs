c8a16d287fbd1a9d6dc563412dc981c1c349735d
James Rampton <me@jamesrampton.com>
2023-03-03 13:56:29 +0000

    Add opentelemetry

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use axum::{response::Html, routing::get, Router};
+use opentelemetry::global;
 use std::net::{Ipv4Addr, SocketAddr};
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    // let subscriber = tracing_subscriber::fmt()
-    //     .with_file(true)
-    //     .with_line_number(true)
-    //     .with_target(true)
-    //     .with_thread_names(true)
-    //     .finish();
-    // tracing::subscriber::set_global_default(subscriber)?;
-    console_subscriber::init();
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name("axum_test")
+        .install_simple()?;
+
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    tracing_subscriber::registry()
+        .with(opentelemetry)
+        .with(fmt::Layer::default())
+        .try_init()?;
 
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 7878));
     axum::Server::bind(&addr)
