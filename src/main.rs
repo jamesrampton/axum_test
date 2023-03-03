6c95176ce51a207e1f79cfc879abcd8d7ab22060
James Rampton <me@jamesrampton.com>
2023-03-03 09:12:54 +0000

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::net::{Ipv4Addr, SocketAddr};
-
 use axum::{response::Html, routing::get, Router};
+use std::net::{Ipv4Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 7878));
+    tracing_subscriber::fmt::init();
     axum::Server::bind(&addr)
         .serve(Router::new().route("/", get(root_get)).into_make_service())
         .await
@@ -12,5 +12,6 @@ async fn main() {
 }
 
 async fn root_get() -> Html<&'static str> {
+    tracing::info!("getting index");
     Html(include_str!("index.html"))
 }
