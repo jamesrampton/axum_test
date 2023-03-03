1cdc1745f4d34e5dcb3af36ad46c91d54696dd97
James Rampton <me@jamesrampton.com>
2023-03-03 10:12:44 +0000

    Make a nice-looking tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ use std::net::{Ipv4Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    let subscriber = tracing_subscriber::fmt()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(true)
+        .with_thread_names(true)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 7878));
-    tracing_subscriber::fmt::init();
     axum::Server::bind(&addr)
         .serve(Router::new().route("/", get(root_get)).into_make_service())
         .await
